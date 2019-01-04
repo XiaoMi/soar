0a94e333d97a8543723e92952c1a9fceceddeaf7
Leon Zhang <martianzhang@hotmail.com>
2019-01-04 16:59:06 +0800

    format main.bats add env

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,5 +1,7 @@
 setup() {
   export SOAR_DEV_DIRNAME="${BATS_TEST_DIRNAME}/../"
+  export SOAR_BIN="${SOAR_DEV_DIRNAME}/bin/soar" 
+  export SOAR_BIN_ENV="${SOAR_DEV_DIRNAME}/bin/soar -config ${SOAR_DEV_DIRNAME}/etc/soar.yaml" 
   export BATS_TMP_DIRNAME="${BATS_TEST_DIRNAME}/tmp"
   export BATS_FIXTURE_DIRNAME="${BATS_TEST_DIRNAME}/fixture"
   mkdir -p "${BATS_TMP_DIRNAME}"
