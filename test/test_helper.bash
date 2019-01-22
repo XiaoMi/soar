dcccebb18d1e870a48025a0b96e0768fac9a3a74
Leon Zhang <martianzhang@hotmail.com>
2019-01-22 23:41:35 +0800

    travis auto build failed debug

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -9,5 +9,5 @@ setup() {
 
 # golden_diff like gofmt golden file check method, use this function check output different with template
 golden_diff() {
-  diff "${BATS_TMP_DIRNAME}/${BATS_TEST_NAME}.golden" "${BATS_FIXTURE_DIRNAME}/${BATS_TEST_NAME}.golden" >/dev/null
+  diff "${BATS_TMP_DIRNAME}/${BATS_TEST_NAME}.golden" "${BATS_FIXTURE_DIRNAME}/${BATS_TEST_NAME}.golden"
 }
